c6201752bef132037f9b69f6c8f7d653377abb04
pavelyakovenko2012 <pavel.yackovencko2012@gmail.com>
2022-09-03 15:24:19 +0500

    Добавили решение(C#)

--- 001/Program.cs
+++ 001/Program.cs
@@ -5,3 +5,15 @@
 // Условие
 // Вывод данных
 // Конец
+
+System.Console.Write("Введите ваше имя:");
+string username = Console.ReadLine()!;
+if (username.ToLower() == "маша")
+{
+    System.Console.WriteLine("Я так ждала тебя, Маша");
+}
+else
+{
+    System.Console.Write("Привет");
+    System.Console.WriteLine(username);
+}
